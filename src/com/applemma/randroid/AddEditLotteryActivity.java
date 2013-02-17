d2f41c60da904da5083fc58d70e774f924bb7cd9
Mahmood Abdulla <mahmood.abdula@gmail.com>
2013-02-17 20:35:34 +0300

    Completed TicketDialogFragment class.

--- src/com/applemma/randroid/AddEditLotteryActivity.java
+++ src/com/applemma/randroid/AddEditLotteryActivity.java
@@ -1,16 +1,36 @@
 package com.applemma.randroid;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 import com.applemma.util.support.DynamicFragmentActivity;
 
-public class AddEditLotteryActivity extends DynamicFragmentActivity
+public class AddEditLotteryActivity extends DynamicFragmentActivity implements
+		TicketDialogFragment.EditTicketDialogListener
 {
+	private static final String DIALOG_FRAGMENT_TAG = "dialog_fragment";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 
 		loadDynamicFragment(new AddEditLotteryFragment());
+	}
+
+	public void showTicketDialog()
+	{
+		String dglTitle = getString(R.string.add_ticket_dlg_title);
+		TicketDialogFragment dlg = TicketDialogFragment.newInstance(dglTitle);
+
+		FragmentManager fm = getSupportFragmentManager();
+		dlg.show(fm, DIALOG_FRAGMENT_TAG);
+	}
+
+	@Override
+	public void onFinishEditTicket(String inputText)
+	{
+		// TODO Auto-generated method stub
+
 	}
 }
--- src/com/applemma/randroid/TicketDialogFragment.java
+++ src/com/applemma/randroid/TicketDialogFragment.java
@@ -6,23 +6,30 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.EditText;
 
 public class TicketDialogFragment extends DialogFragment implements
 		DialogInterface.OnClickListener
 {
+	private static final String KEY_TITLE = "title";
 	private View mForm;
-	private String mDialogTitle;
 
 	public interface EditTicketDialogListener
 	{
 		void onFinishEditTicket(String inputText);
 	}
 
-	public TicketDialogFragment(String dialogTitle)
+	public static TicketDialogFragment newInstance(String dialogTitle)
 	{
-		mDialogTitle = dialogTitle;
-	}
+		TicketDialogFragment f = new TicketDialogFragment();
+		Bundle args = new Bundle();
+		
+		args.putString(KEY_TITLE, dialogTitle);
+		f.setArguments(args);
 
+		return f;
+	}
+	
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState)
 	{
@@ -31,20 +38,24 @@ public class TicketDialogFragment extends DialogFragment implements
 
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-		AlertDialog dialog = 
-				builder.setTitle(mDialogTitle)
-					   .setView(mForm)
-					   .setPositiveButton(android.R.string.ok, this)
-					   .setNegativeButton(android.R.string.cancel, null)
-					   .create();
-		
+		AlertDialog dialog = builder
+				.setTitle(getArguments().getString(KEY_TITLE))
+				.setView(mForm)
+				.setPositiveButton(android.R.string.ok, this)
+				.setNegativeButton(android.R.string.cancel, null)
+				.create();
+
 		return dialog;
 	}
 
 	@Override
 	public void onClick(DialogInterface dialog, int which)
 	{
-		// TODO
-	}
+		EditText editText = (EditText) mForm
+				.findViewById(R.id.ticket_title_edit);
+		String inputText = editText.getText().toString();
 
+		EditTicketDialogListener activity = (EditTicketDialogListener) getActivity();
+		activity.onFinishEditTicket(inputText);
+	}
 }
